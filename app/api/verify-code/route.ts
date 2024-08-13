bc75826ede4d7712b75239d348dfdc7f87206cc1
Prateek2593 <patidarprateek2593@gmail.com>
2024-08-13 13:12:59 +0530

    otp verification and unique username check

---
+++ app/api/verify-code/route.ts
@@ -0,0 +1,66 @@
+// api/verify-code/route.ts
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  try {
+    const { username, code } = await request.json();
+
+    //url decoding
+    const decodedUsername = decodeURIComponent(username);
+
+    const user = await UserModel.findOne({ username: decodedUsername });
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 500 }
+      );
+    }
+
+    const isCodeValid = user.verifyCode === code;
+    const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+
+    if (isCodeValid && isCodeNotExpired) {
+      user.isVerified = true;
+      await user.save();
+      return Response.json(
+        {
+          success: true,
+          message: "User verified successfully",
+        },
+        { status: 200 }
+      );
+    } else if (!isCodeNotExpired) {
+      return Response.json(
+        {
+          success: false,
+          message:
+            "Verification Code expired!! Please signup again to get new code",
+        },
+        { status: 400 }
+      );
+    } else {
+      return Response.json(
+        {
+          success: false,
+          message: "Incorrect verification code",
+        },
+        { status: 400 }
+      );
+    }
+  } catch (error) {
+    console.error("Error in verifying user", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error in verifying user",
+      },
+      { status: 500 }
+    );
+  }
+}
